064d7fa78bb32d7b3e80bb9930acd621eeae4b78
santilara <santilaracelis16@gmail.com>
2023-05-20 14:17:22 +0200

    ADD EjemploComponente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { EjemploComponente } from './components/EjemploComponente';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <EjemploComponente/>
       </header>
     </div>
   );
